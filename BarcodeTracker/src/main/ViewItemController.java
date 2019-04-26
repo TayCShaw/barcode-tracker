2b32244871c7ad3fde87312495294ff13b99b5f4
TShaw <47929530+TayCShaw@users.noreply.github.com>
2019-04-25 21:22:54 -0400

    Removed unused imports.

--- BarcodeTracker/src/main/ViewItemController.java
+++ BarcodeTracker/src/main/ViewItemController.java
@@ -1,17 +1,14 @@
 package main;
 
 import java.io.IOException;
-import java.net.URL;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ResourceBundle;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
